177428abd3d4a1ee1b6c00c86b6edd313412179a
FrankRudolph <118888723+FrankRudolph83@users.noreply.github.com>
2023-01-03 20:30:18 +0100

    Add files via upload
    Das Programm weist zwei Fragezeichen (?) auf. Ersetze diese durch die passende Bedingung.

---
+++ 135_Quadratwurzel.py
@@ -0,0 +1,18 @@
+#-------------------------------------------------------------
+# Berechnung der Quadratwurzel
+#
+#Autor:   Dein Name
+#Datum:   Aktuelles Datum
+#-------------------------------------------------------------
+
+from math import sqrt
+
+zahl = float(input("Bitte eine Zahl eingeben: "))
+
+?
+    wurzel = sqrt(zahl)
+    print("Die Wurzel aus %f ist %f " % (zahl, wurzel))
+?
+    print("Aus einer negativen Zahl kann keine")
+    print("Quadratwurzel berechnet werden.")
+    print("Geben Sie eine positive Zahl ein!")
